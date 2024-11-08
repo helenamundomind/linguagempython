1492cf18edb8333c84275ddb17e4464a45c1e661
Helena Santos <helenabezerra962@gmail.com>
2024-11-08 19:58:59 -0300

    Update css.py

--- css.py
+++ css.py
@@ -1,1 +1,26 @@
+## Codigod básicos para a linguagem css
+body { 
+ banchground-color: ligntblue;
+}
+--- Alterar a cor e o tamanho do texto
+p {
+   color: darkgreen;
+   front-sinza: 16px;
+}
+--- Alterar o estilo da borda
+div {
+    border: 2px solid black;
+    padding: 10px;
+--- Alinhar texto
+h1 {
+  windth: 300px;
+  height: 200px;
+
+
+
+
+
+
+
+
 
